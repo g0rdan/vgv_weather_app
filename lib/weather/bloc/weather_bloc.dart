0ceb791e845a6362f360bd59e9f387c14990f71f
Denis <gordin.dan@gmail.com>
2021-03-10 07:43:45 -0500

    add empty blocs and cubits

---
+++ lib/weather/bloc/weather_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'weather_event.dart';
+part 'weather_state.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  WeatherBloc() : super(WeatherInitial());
+
+  @override
+  Stream<WeatherState> mapEventToState(
+    WeatherEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
